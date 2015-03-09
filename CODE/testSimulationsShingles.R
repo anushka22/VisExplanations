ea85b03747ae4859974029b09dc2a820b8e378aa
anushka22 <anushka.a22@gmail.com>
2015-03-09 14:21:57 -0700

    Added Shingling code (doesn’t produce correct images yet) And some changes to intro and related work..

--- CODE/testSimulationsShingles.R
+++ CODE/testSimulationsShingles.R
@@ -45,47 +45,70 @@ getScagnosticsMeasure <- function(d, variables){
 	})
 }
 #--------
-getPartitionScores <- function(d, partitioner, variables, r1, r2, N, scoreName){
-	switch(scoreName,
-		jent={
-			groupS <- ddply(d, partitioner, .fun=function(x,col){ e=getEntropyMeasure(x,col,r1,r2, N)}, variables)
-		},
-		scag={
-			groupS <- ddply(d, partitioner, .fun=function(x, col){s= getScagnosticsMeasure(x, col)}, variables)
-		},
-		msr={
-			groupS <- ddply(d, partitioner, .fun=function(x,col){ e=getMSRMeasure(x,col)}, variables)
-		},
-		stop("Enter something that switches me!")
-	)
+getPartitionScores <- function(d, pIndices, variables, r1, r2, N, scoreName){
+	groupS <- NULL
+	for (i in 1:dim(pIndices)[1]){
+		sset = d[pIndices[i,1]:pIndices[i,2],]
+		switch(scoreName,
+			jent={
+				groupS <- rbind(groupS, getEntropyMeasure(sset,variables,r1,r2, N))
+			},
+			scag={
+				groupS <- rbind(groupS, getScagnosticsMeasure(sset, variables))
+			},
+			msr={
+				groupS <- rbind(groupS, getMSRMeasure(sset,variables))
+			},
+			stop("Enter something that switches me!")
+		)
+	}
 	return(groupS)
 }
 #--------
-savePartitionData <- function(d, partitioner, variables, scoreName, groupS, rankValue, dscores, oscores){
-	formula <- paste(partitioner, "~ .",sep=" ")
-	allPlots <- ggplot(d, aes_string(x=variables[1], y=variables[2])) + geom_point(shape=1) + theme(aspect.ratio = 1)
-	subPlots <- allPlots + facet_grid(formula)
+savePartitionData <- function(d, partitioner, variables, scoreName,rankValue, dscores, oscores, pIndices){
+	nplots <- dim(pIndices)[1]
+	ht <- 2*length(nplots)
+	pplots = list()
+	for (i in 1:nplots){
+		sset = d[pIndices[i,1]:pIndices[i,2],]
+		name = paste("V",i,sep="")
+		pplots[[name]] <- ggplot(sset, aes_string(x=variables[1], y=variables[2])) + geom_point(shape=1) + theme(aspect.ratio = 1)
+	}
+#	subPlots <- do.call("grid.arrange", c(pplots,  ncol=1, nrow=nplots))
 
-	a = data.frame(cbind(dscores, partitioner=as.character(levels(d[[partitioner]]))))
+	a = data.frame(cbind(dscores, partitioner=letters[1:nplots]))
 	mmin = NULL
 	mmax = NULL
 	for (i in 1:dim(dscores)[1]){
-		mmin = rbind(mmin, min(oscores[i,1], min(dscores[i,])))
-		mmax = rbind(mmax, max(oscores[i,1], max(dscores[i,])))
+		mmin = rbind(mmin, min(oscores[i], min(dscores[i,])))
+		mmax = rbind(mmax, max(oscores[i], max(dscores[i,])))
 	}
 	a= melt(a, id="partitioner")
 	names(a)[which(names(a)=="partitioner")] = partitioner
 	a$value = as.numeric(a$value)
-	oscores = data.frame(cbind(oscores, partitioner=as.character(levels(d[[partitioner]]))))
-	names(oscores)[which(names(oscores)=="partitioner")] = partitioner
+	oscores = data.frame(cbind(oscores, partitioner=letters[1:nplots]))
+	names(oscores) = c("values", partitioner)
 
-	allHists <- ggplot(a, aes(x=value)) + geom_histogram() + geom_vline(data=oscores, aes(xintercept=as.numeric(allScores..v..)), linetype=3, col="red") + geom_text(data=oscores, aes(x=as.numeric(allScores..v..),y=0,label=as.character(round(allScores..v.., 2))), col="red", size=3) + ggtitle(paste("Partitioned by: ",partitioner,sep="")) 
-	subHists <- allHists + facet_grid(formula)
+	#---TODO ---this isn't plotting the correct vlines :S
+	formula =paste(partitioner, "~ .",sep=" ")
+	allHists <- ggplot(a, aes(x=value)) + geom_histogram() + ggtitle(paste("Partitioned by: ",partitioner,sep="")) 
+	subHists <- allHists + facet_grid(formula, scales="free")
 	subHists
+	subHists <- subHists + geom_vline(data=oscores, aes(xintercept=as.numeric(values)), linetype=3, col="red") + geom_text(data=oscores, aes(x=as.numeric(values),y=0,label=as.character(values)), col="red", size=3) 
 	
-	ht <- 2*length(levels(d[[partitioner]]))
-	g <-arrangeGrob(subPlots, subHists, ncol=2, widths=c(4,4), heights=c(ht,ht))
-	ggsave(paste(path,"simsMaxAbs/",scoreName,"/", rankValue, "-", partitioner, "-", variables[1], "-", variables[2], ".pdf", sep=""), g, width=8,height=ht)
+	#---TODO haven't figured out how to plot these together..
+	fpath=paste(path,"simsMaxAbs/",scoreName,"/", rankValue, "-", partitioner, "-", variables[1], "-", variables[2], ".pdf", sep="")
+	print(fpath)
+	pdf(fpath)
+	grid.newpage() # Open a new page on grid device
+	pushViewport(viewport(layout = grid.layout(nplots, 1)))
+	for (i in 1:nplots){
+		print(pplots[i], vp = viewport(layout.pos.row = i, layout.pos.col = 1))
+	}
+	print(subHists, vp = viewport(layout.pos.row = 1:nplots, layout.pos.col = 2:2))
+	dev.off()
+	#g <-arrangeGrob(subPlots, subHists, ncol=2, widths=c(4,4), heights=c(ht,ht))
+	#ggsave(, g, width=8,height=ht)
 }
 
 #----------------
@@ -109,6 +132,21 @@ computeKDE <- function(npts, nsplits, x, ptInterest){
 	}
 }
 #----------------
+computeShingles <- function(d, partitioner){
+	t=d[complete.cases(d),]
+	z = equal.count(d[[partitioner]])
+	plot(z)
+	t=levels(z)
+	s <- sort(d[[partitioner]])
+	inds <- NULL
+	for (i in 1:length(t)){
+		ss = which(s > t[[i]][1] & s < t[[i]][2])
+		inds <- rbind(inds, c(ss[1], ss[length(ss)]))
+	}
+	return(inds)
+}
+
+#----------------
 computeAllScores <- function(dall, d, partitioner, variables){
 	scNames = c("scag")#, "jent", "msr" )
 	NumSamples <- 100
@@ -119,14 +157,14 @@ computeAllScores <- function(dall, d, partitioner, variables){
 		r1 <- range(dall[[variables[1]]], na.rm=TRUE)
 		r2<- range(dall[[variables[2]]], na.rm=TRUE)
 		N <- dim(dall)[1]
+
+		#---split by known partitioner and save scores
+		pIndices <- computeShingles(d, partitioner)
+		allScores <- getPartitionScores(d, pIndices, variables, r1, r2, N, scoreName)
 		#---Get known partitioner split sizes
-		tab <- table(d[[partitioner]], useNA="ifany")
-		sizes <- sapply(tab, function(x) x[1])
+		sizes <- pIndices[,2]-pIndices[,1] + 1
 		cat("SIZES: ",sizes, "\n")
 		
-		#---split by known partitioner and save scores
-		d[[partitioner]] <-factor(d[[partitioner]],exclude=NULL)
-		allScores <- getPartitionScores(d, partitioner, variables, r1, r2, N, scoreName)
 		if (scoreName == "msr"){
 			vs = c( "mean", "stdev", "r2") 
 		}else if (scoreName == "scag"){
@@ -137,18 +175,11 @@ computeAllScores <- function(dall, d, partitioner, variables){
 		
 		#---get scores from simulations
 		scores <-list()		#---each row is one split's worth of sim scores
-		#---get scores aligned
-		#splitNames=NULL
-		on=names(sort(sizes))
-		allScores = allScores[match(on, allScores[[partitioner]]),]
 		for (i in 1:NumSamples){
-			eg <- assignToRandomKGroupsBySize(d, sizes)
-			eg <- eg[,-which(names(eg)== partitioner)]
-			names(eg)[which(names(eg)=="as.factor(gnum)")] <- "randCluster"
-			partScores <- getPartitionScores(eg, "randCluster", variables, r1, r2, N, scoreName)
-			#plot(eg[which(eg$randCluster==6),1:2])
-			#partScores
-			#splitNames = partScores$randCluster
+			#---randomly permute
+			nrows <- dim(d)[1]
+			eg<-d[sample(1:nrows,nrows,replace=FALSE),]
+			partScores <- getPartitionScores(eg, pIndices, variables, r1, r2, N, scoreName)
 			for (v in vs){
 				scores[[v]] <- cbind(scores[[v]], partScores[[v]])
 			}
@@ -157,20 +188,12 @@ computeAllScores <- function(dall, d, partitioner, variables){
 		zscores = list()
 		#---loop through the score histograms
 		for (v in vs){
-			#saveScoreHistograms(v, scores, allScores, partitioner, variables, allScores[[partitioner]])
 			mu = apply(scores[[v]],1, mean, na.rm=TRUE)
 			sdev = apply(scores[[v]],1, sd, na.rm=TRUE)
 			zscores[[v]] <- max(abs(allScores[[v]] - mu)/sdev, na.rm=TRUE)
 			cat("VAR: ",partitioner,"\nv: " , v, " mean: " ,mu, " sdev ", sdev, " zscore: " , zscores[[v]],"\n")
 			#---save partition + ranking
-			pdata <- allScores[,c(partitioner,v)]
-			pdata <- cbind(pdata, variable = rep(v, dim(allScores)[1]))
-			pdata$variable <- as.factor(pdata$variable)
-			names(pdata)[which(names(pdata) == v)] <- "value"
-			savePartitionData(d, partitioner, variables, v, pdata, zscores[[v]], scores[[v]], as.data.frame(allScores[[v]]))
-			
-#			x <- scores[[v]]
-#			kde <- computeKDE(dim(x)[2], dim(x)[1], x, allScores[[v]])
+			savePartitionData(d, partitioner, variables, v, zscores[[v]], scores[[v]], allScores[[v]], pIndices)
 		}
 	}	#---for scoreName
 }
@@ -182,8 +205,9 @@ computeAllScores <- function(dall, d, partitioner, variables){
 path <- "/Users/aanand/Documents/RESEARCH Notes/RobustScagnostics/"
 datafiles <- c("ourworld.csv", "IPEDS_data.csv", "genClustered.csv", "genDonut.csv", "genMonotonic.csv", "genQuadratic.csv", "genSerial.csv", "genStriatedChopped.csv", "genStriatedLines.csv", "adult.csv", "baseball2004.csv", "BOSTON.csv", "Sample - Superstore Sales.csv")
 
-datafiles <- c("IPEDS_data.csv")#c("baseball2004.csv")
-for (df in datafiles){
+datafiles <- c("baseball2004.csv")
+df = datafiles[1]
+#for (df in datafiles){
 	fname <- paste(path, "data/", df, sep="")
 	dall <- read.csv(fname, header=TRUE)
 
@@ -191,38 +215,26 @@ for (df in datafiles){
 	bs=ifelse(grepl("(bin)",names(dall)), TRUE, ifelse(grepl("(cluster)",names(dall)), TRUE, FALSE))
 	if (length(bs[which(bs==TRUE)]) > 0)
 		dall[[names(dall)[bs]]] <- as.factor(dall[[names(dall)[bs]]])
-	#---get list of partitioners
+	#---remove factors
 	dfactors <- sapply(dall, is.factor)
 	ps <- names(dall[dfactors])
-	partitioners <- vector()
-	for (i in 1:length(ps)){
-		splits <- split(dall,dall[[ps[i]]])
-		avgSplitSize <- mean(sapply(splits, function(x) dim(x)[1])) 
-		prop <- dim(dall)[1]/5
-		#---for factors that have small cardinality
-		#---and split the dataset into relatively big chunks
-		if (length(levels(dall[,ps[i]])) < dim(dall)[1]/2 & avgSplitSize >= prop)
-			partitioners <- c(partitioners, ps[i])
-	}
-	allVars <- names(dall)[!(names(dall) %in% partitioners)]
+	allVars <- names(dall)[!(names(dall) %in% ps)]
 	
-	#---for all pairs of vars and partitioners
-	#allVars <- c("DEATH_RT", "BIRTH_RT")
-	allVars <- c("Percent.admitted...total" ,"Graduation.rate...Bachelor.degree.within.6.years..total")
-	#allVars <- c("PutOutRate", "AssistRate")
+	#---for all pairs of vars that are Qs
 	for (i in 1:length(allVars)){
 		for (j in (i+1):length(allVars)){
 			if (j > length(allVars))
 				break
-			for (partitioner in partitioners){
-				if (length(levels(dall[[partitioner]])) <= 1){
+			for (k in 1:length(allVars)){
+				if (k == i | k== j)
 					next
-				}
+				partitioner=allVars[k]
 				variables = c(allVars[i], allVars[j])
 				cat("Processing: ", variables, " and partitioner: ", partitioner, "\n")
 				d <- dall[,c(variables,partitioner)]
 				computeAllScores(dall, d, partitioner, variables)
-			}
-		}
-	}
-}
+			}	#---for k
+		}	#---for j
+	}	#---for i
+#}
+
